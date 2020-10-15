9bdffc2c480031eb6e0e22761ac2a766e6321bc6
vishnukt <kt.vishnu19@gmail.com>
2020-10-15 16:07:03 +0530

    Added / to all_results - Error Fix

--- learn_django/mysite/polls/urls.py
+++ learn_django/mysite/polls/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('all_results', views.all_results.as_view(), name='all_results')
+    path('all_results/', views.all_results.as_view(), name='all_results')
 ]

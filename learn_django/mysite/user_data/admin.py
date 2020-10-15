f47dd06155f2a94603541d44c844a3448c5cd1cf
vishnukt <kt.vishnu19@gmail.com>
2020-10-15 16:08:58 +0530

    Imported User model

--- learn_django/mysite/user_data/admin.py
+++ learn_django/mysite/user_data/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import user_data, User
+from django.contrib.auth.models import User
+
+from .models import user_data
 
 admin.site.register(user_data)

b7dc1d1e6886746591c6bbf16a0ee4d467ff76a5
vishnukt <kt.vishnu19@gmail.com>
2020-10-21 02:16:22 +0530

    Added new app Rest Api

--- learn_django/mysite/mysite/urls.py
+++ learn_django/mysite/mysite/urls.py
@@ -14,14 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+
 from django.urls import include, path
+
+from rest_framework import routers
+
+from restapi import views
+
 from django.conf.urls import url
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'userdata', views.UserDataView)
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('user_data/', include('user_data.urls')),
     path('admin/', admin.site.urls),
     path('polls/accounts/', include('django.contrib.auth.urls')),
-    # path('accounts', include('django.contrib.auth.urls')),
-    url(r"^accounts/", include("django.contrib.auth.urls")),
+    # url(r"^polls/accounts/", include("django.contrib.auth.urls")),
+    # url(r'^restapi/', include(restapi.urls, namespace="restapi")),
+    path('restapi/', include('restapi.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('restapi/test/', include(router.urls)),
 ]

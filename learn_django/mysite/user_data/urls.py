895f204e157b77f75efac7fedbfccbd5f3b395bc
vishnukt <kt.vishnu19@gmail.com>
2020-10-21 02:20:14 +0530

    Removed Comments

--- learn_django/mysite/user_data/urls.py
+++ learn_django/mysite/user_data/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-# from django.conf.urls import url
-
 from . import views
 
 app_name = 'user_data'
@@ -10,8 +8,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('view_user_details', views.view_details.as_view(), name='view'),
     path('input_user_details', views.get_name, name='input'),
-    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # path('<int:question_id>/vote/', views.vote, name='vote'),
-    # path('all_results', views.all_results.as_view(), name='all_results')
 ]

bb84e8413a78cb2ff2c44df104ede01667464594
vishnukt <kt.vishnu19@gmail.com>
2020-10-22 03:16:08 +0530

    Added an blank space - flake8 standards

--- learn_django/mysite/user_data/models.py
+++ learn_django/mysite/user_data/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
 class user_data(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=200)

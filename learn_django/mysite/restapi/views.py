026baaf7ea94436b14d35ee656759be2d520b136
vishnukt <kt.vishnu19@gmail.com>
2020-10-23 03:51:45 +0530

    Completed Create, Update, Delete in APIView

--- learn_django/mysite/restapi/views.py
+++ learn_django/mysite/restapi/views.py
@@ -84,51 +84,70 @@ class UserDataApiView(APIView):
     serializer_class = UserModelSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def get(self, request, format=None):
-        return Response({self.queryset.all()})
+    def get(self, request, format=None, pk=None):
+        if pk is None:
+            return Response({self.queryset.all()})
+        else:
+            return Response({self.queryset.filter(pk=pk)})
 
-    # def create(self, request):
-    #     serializer = self.serializer_class(data=request.data)
+    def post(self, request, pk=None):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            n = serializer.validated_data.get('full_name')
+            t = serializer.validated_data.get('home_town')
+            a = serializer.validated_data.get('age')
+            u = user_data(full_name=n, home_town=t, age=a)
+            u.user = request.user
+            u.save()
+            message = f'{u.user} Data Succesfully Saved!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-    #     if serializer.is_valid():
-    #         n = serializer.validated_data.get('full_name')
-    #         t = serializer.validated_data.get('home_town')
-    #         a = serializer.validated_data.get('age')
-    #         u = user_data(full_name=n, home_town=t, age=a)
-    #         u.user = request.user
-    #         u.save()
-    #         message = f'{u.user} Data Succesfully Saved!'
-    #         return Response({'message': message})
-    #     else:
-    #         return Response(
-    #             serializer.errors,
-    #             status=status.HTTP_400_BAD_REQUEST
-    #         )
+    def put(self, request, pk=None):
+        serializer = self.serializer_class(data=request.data)
+        if pk is not None:
+            if serializer.is_valid():
+                n = serializer.validated_data.get('full_name')
+                t = serializer.validated_data.get('home_town')
+                a = serializer.validated_data.get('age')
+                # u = user_data(full_name=n, home_town=t, age=a)
+                user_data.objects.filter(id=pk).update(full_name=n, home_town=t, age=a)
+                message = 'Data Succesfully Updated!'
+                return Response({'message': message})
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'Failed! : Select only a specific data'})
 
-    # # def retrieve(self, request, pk=None):
-    # #     pass
+    def patch(self, request, pk=None):
+        serializer = self.serializer_class(data=request.data)
+        if pk is not None:
+            if serializer.is_valid():
+                n = serializer.validated_data.get('full_name')
+                t = serializer.validated_data.get('home_town')
+                a = serializer.validated_data.get('age')
+                # u = user_data(full_name=n, home_town=t, age=a)
+                if n is None:
+                    n = self.queryset.filter(pk=pk).values('full_name')
+                if t is None:
+                    t = self.queryset.filter(pk=pk).values('home_town')
+                if a is None:
+                    a = self.queryset.filter(pk=pk).values('age')
+                user_data.objects.filter(id=pk).update(full_name=n, home_town=t, age=a)
+                message = 'Data Succesfully Updated!'
+                return Response({'message': message})
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'Failed! : Select only a specific data'})
 
-    # def update(self, request, pk=None):
-    #     """Handle updating an object"""
-    #     serializer = self.serializer_class(data=request.data)
-
-    #     if serializer.is_valid():
-    #         n = serializer.validated_data.get('full_name')
-    #         t = serializer.validated_data.get('home_town')
-    #         a = serializer.validated_data.get('age')
-    #         # u = user_data(full_name=n, home_town=t, age=a)
-    #         user_data.objects.filter(id=pk).update(full_name=n, home_town=t, age=a)
-    #         message = 'Data Succesfully Updated!'
-    #         return Response({'message': message})
-    #     else:
-    #         return Response(
-    #             serializer.errors,
-    #             status=status.HTTP_400_BAD_REQUEST
-    #         )
-
-    # # def partial_update(self, request, pk=None):
-    # #     return Response({'http_method': 'PATCH'})
-
-    # def destroy(self, request, pk=None):
-    #     user_data.objects.filter(id=pk).delete()
-    #     return Response({'SUCCESSFULLY DELETE'})
+    def delete(self, request, pk=None):
+        if pk is not None:
+            user_data.objects.filter(id=pk).delete()
+            return Response({'SUCCESSFULLY DELETE'})
+        else:
+            return Response({'Failed! : Select only a specific data'})
